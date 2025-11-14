efeec821c6b50f4e3193daf786e2093788bf797c
Md. Arifur Rahman <rimel5246@gmail.com>
2025-11-15 00:42:47 +0600

    simple math done

---
+++ simpleMath.js
@@ -0,0 +1,21 @@
+// minimum number find
+const min =Math.min(45, 34,85,75,83,76,456);
+console.log(min)
+
+let max = Math.max(845,455,122,752,665,68,685,854,52);
+console.log(max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(3-10))
+
+console.log(Math.round(65.59825))
+
+console.log(Math.floor(65.9825))
+
+console.log(Math.ceil(65.59825))
+
+console.log(Math.random()*10)
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
