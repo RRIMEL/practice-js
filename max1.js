448456e57524c594f2d1fdaead9382d9f0204fab
Md. Arifur Rahman <rimel5246@gmail.com>
2025-11-17 19:59:25 +0600

    max 1 practice done

---
+++ max1.js
@@ -0,0 +1,18 @@
+const kim = 34;
+const tim= 445;
+const jim = 62;
+
+if(kim > tim && kim > jim){
+    console.log('kim is ultimate boss');
+}
+else if(tim > jim && tim > kim){
+    console.log('tim is ultimate boss');
+}
+else{
+    console.log('jim is thhe boss');
+}
+
+// otherwise
+
+const max = Math.max(24, 34,63,64,34,34,76,);
+console.log('max number is :', max)
